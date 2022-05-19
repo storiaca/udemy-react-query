128e7b51778a3b65f82c4a98f35cae951d35bcad
Aleksandar Ristic <storiaca@gmail.com>
2022-05-19 08:51:04 +0200

    Fallback data

--- lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
+++ lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,6 +12,7 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
-  const {data} = useQuery(queryKeys.treatments, getTreatments)
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments)
   return data;
 }
