eb5fd301853d34faa647ca3bbd14b0606f5cad34
Aleksandar Ristic <storiaca@gmail.com>
2022-06-02 21:07:08 +0200

    Update re-fetch options

--- lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
+++ lazy-days-spa/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,21 +1,30 @@
 import { useQuery, useQueryClient } from "react-query";
-import type { Treatment } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
+import type { Treatment } from "../../../../../shared/types";
+import { axiosInstance } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get('/treatments');
+  const { data } = await axiosInstance.get("/treatments");
   return data;
 }
 
 export function useTreatments(): Treatment[] {
   // get data from server via useQuery
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments)
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes (doesen't make sense for staleTime to exceed cacheTime)
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments)
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 600000, // 10 minutes
+    cacheTime: 900000, // 15 minutes (doesen't make sense for staleTime to exceed cacheTime)
+  });
 }
