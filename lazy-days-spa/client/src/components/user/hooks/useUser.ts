0cbdd8bc1cff3c4c8943b2d7f0813d6f59f032c8
Aleksandar Ristic <storiaca@gmail.com>
2022-06-04 16:12:22 +0200

    useAuth, useUser and useQuery

--- lazy-days-spa/client/src/components/user/hooks/useUser.ts
+++ lazy-days-spa/client/src/components/user/hooks/useUser.ts
@@ -1,24 +1,25 @@
-import { AxiosResponse } from 'axios';
+import { useQuery } from "react-query";
+import { AxiosResponse } from "axios";
 
-import type { User } from '../../../../../shared/types';
-import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
+import type { User } from "../../../../../shared/types";
+import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
 import {
   clearStoredUser,
   getStoredUser,
   setStoredUser,
-} from '../../../user-storage';
+} from "../../../user-storage";
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    }
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,8 +28,8 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  // call useQuery to update user data from server
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
