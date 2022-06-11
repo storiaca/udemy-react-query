304dc8999b76b69bfe4776c1e03d75b99336f7f7
Aleksandar Ristic <storiaca@gmail.com>
2022-06-11 13:18:59 +0200

    Writing optimistic update

--- lazy-days-spa/client/src/components/user/hooks/usePatchUser.ts
+++ lazy-days-spa/client/src/components/user/hooks/usePatchUser.ts
@@ -1,9 +1,10 @@
-import { useMutation, UseMutateFunction } from "react-query";
+import { useMutation, UseMutateFunction, useQueryClient } from "react-query";
 import jsonpatch from "fast-json-patch";
 
 import type { User } from "../../../../../shared/types";
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
+import { queryKeys } from "../../../react-query/constants";
 import { useCustomToast } from "../../app/hooks/useCustomToast";
 
 // for when we need a server function
@@ -35,18 +36,47 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUserData: User) => patchUserOnServer(newUserData, user),
     {
+      // onMutate returns context that is passed to onError
+      onMutate: async (newData: User | null) => {
+        // cancel any outgoing queries for user data, so old
+        // server data doesen't overwrite our optimistic update
+        queryClient.cancelQueries(queryKeys.user);
+
+        // snapshot of previous user value
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+
+        // optimistically update the cache with new user value
+        updateUser(newData);
+
+        // return context object with snapshotted value
+        return { previousUserData };
+      },
+      onError: (error, newData, context) => {
+        // rollback cache to saved value
+        if (context.previousUserData) {
+          updateUser(context.previousUserData);
+          toast({
+            title: "Update failed; restoring previous values",
+            status: "warning",
+          });
+        }
+      },
       onSuccess: (userData: User | null) => {
         if (user) {
-          updateUser(userData);
           toast({
             title: "User updated!",
             status: "success",
           });
         }
+      },
+      onSettled: () => {
+        // invalidate user query to make sure we're in sync with server data
+        queryClient.invalidateQueries(queryKeys.user);
       },
     }
   );
