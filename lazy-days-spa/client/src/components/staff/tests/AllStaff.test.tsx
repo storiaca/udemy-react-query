42a69df9ae7ecfae72491917a6034f613c792ff5
Aleksandar Ristic <storiaca@gmail.com>
2022-06-13 08:21:51 +0200

    Test Rendered staff data

--- lazy-days-spa/client/src/components/staff/tests/AllStaff.test.tsx
+++ lazy-days-spa/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,16 +1,23 @@
-import { screen } from '@testing-library/react';
+import { screen } from "@testing-library/react";
 
 // import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
-// import { renderWithClient } from '../../../test-utils';
-import { AllStaff } from '../AllStaff';
+import { server } from "../../../mocks/server";
+import { renderWithQueryClient } from "../../../test-utils";
+import { AllStaff } from "../AllStaff";
 
-test('renders response from query', () => {
+test("renders response from query", async () => {
   // write test here
+  renderWithQueryClient(<AllStaff />);
+
+  const staffTitles = await screen.findAllByRole("heading", {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(staffTitles).toHaveLength(4);
 });
 
-test('handles query error', async () => {
+test("handles query error", async () => {
   // (re)set handler to return a 500 error for staff
   // server.resetHandlers(
   //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
